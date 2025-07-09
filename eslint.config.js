e430c7e5a21789e11a1b74874fd9f3c38500a201
Thiago Nunes <thiago.luno@gmail.com>
2025-07-09 00:58:00 -0300

    add theme config

--- eslint.config.js
+++ eslint.config.js
@@ -14,10 +14,32 @@ export default tseslint.config([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
-  },
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      eqeqeq: ['error', 'always'],
+      'no-duplicate-imports': 'error',
+      curly: ['error', 'all'],
+      'no-nested-ternary': 'error',
+      'default-case': 'warn',
+      'no-magic-numbers': [
+        'warn',
+        { ignore: [0, 1, -1], ignoreArrayIndexes: true, enforceConst: true }
+      ],
+      complexity: ['warn', 10],
+      'max-depth': ['warn', 4],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/explicit-function-return-type': 'warn',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-inferrable-types': 'warn'
+    }
+  }
 ])
